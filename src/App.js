58bc17fddda39e4413248cb1a1fcf0c042107aba
Ahnaf Sadik Inan <62875018+Hartz-I@users.noreply.github.com>
2020-12-19 17:32:30 +0600

    Revert "trying out some stuff"
    This reverts commit cadfc698bf976b50e5179d7262cd5cdbb29256ff.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-import React from "react";
-import logo from "./logo-borgomul-triangle.svg";
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Navigation from "./components/Navigation/Navigation";
-import SideBar from "./components/SideBar/SideBar";
-import { AsyncMode } from "react-is";
+import React from 'react';
+import logo from './logo-borgomul-triangle.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <div className="background-title">বর্গমূল</div>
+        <div>
+          বর্গমূল
+        </div>
       </header>
-      <Navigation />
-      <SideBar />
     </div>
   );
 }
