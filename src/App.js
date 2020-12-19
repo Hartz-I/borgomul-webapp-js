cadfc698bf976b50e5179d7262cd5cdbb29256ff
Ahnaf Sadik Inan <62875018+Hartz-I@users.noreply.github.com>
2020-12-19 17:30:10 +0600

    trying out some stuff

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React from 'react';
-import logo from './logo-borgomul-triangle.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo-borgomul-triangle.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navigation from "./components/Navigation/Navigation";
+import SideBar from "./components/SideBar/SideBar";
+import { AsyncMode } from "react-is";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <div>
-          বর্গমূল
-        </div>
+        <div className="background-title">বর্গমূল</div>
       </header>
+      <Navigation />
+      <SideBar />
     </div>
   );
 }
